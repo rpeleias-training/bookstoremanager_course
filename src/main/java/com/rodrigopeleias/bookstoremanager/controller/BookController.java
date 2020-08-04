50853e8189a7735d439174c39cf4e0afc94a9e88
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-08-04 08:50:02 -0300

    First commit

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/controller/BookController.java
@@ -0,0 +1,15 @@
+package com.rodrigopeleias.bookstoremanager.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/books")
+public class BookController {
+
+    @GetMapping
+    public String hello() {
+        return "Hello Bookstore Manager";
+    }
+}
