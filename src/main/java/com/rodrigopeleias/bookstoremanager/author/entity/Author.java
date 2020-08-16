f2c85e8fcd95e5c484394b1366c01e9e0ee16cbb
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-08-16 18:38:34 -0300

    Created model mappings

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/author/entity/Author.java
@@ -0,0 +1,31 @@
+package com.rodrigopeleias.bookstoremanager.author.entity;
+
+import com.rodrigopeleias.bookstoremanager.books.entity.Book;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.List;
+
+@Data
+@Entity
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(columnDefinition = "integer default 0")
+    private int age;
+
+    @OneToMany(mappedBy = "author", fetch =  FetchType.LAZY)
+    private List<Book> books;
+}
---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/users/entity/User.java
@@ -0,0 +1,51 @@
+package com.rodrigopeleias.bookstoremanager.users.entity;
+
+import com.rodrigopeleias.bookstoremanager.books.entity.Book;
+import com.rodrigopeleias.bookstoremanager.users.enums.Gender;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.List;
+
+@Data
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
+    private Gender gender;
+
+    @Column(nullable = false, unique = true, length = 100)
+    private String email;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, columnDefinition = "TIMESTAMP")
+    private LocalDate birthDate;
+
+    @OneToMany(mappedBy = "user", fetch =  FetchType.LAZY)
+    private List<Book> books;
+}
