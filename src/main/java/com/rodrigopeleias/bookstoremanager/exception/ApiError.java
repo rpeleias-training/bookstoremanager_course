8acd08c209b6f508f08f179eb790496d79585ebc
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-08-19 09:34:13 -0300

    Developed Exception Handler for our project

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/exception/ApiError.java
@@ -0,0 +1,28 @@
+package com.rodrigopeleias.bookstoremanager.exception;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ApiError {
+
+    private int code;
+
+    private String status;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    private LocalDateTime timestamp;
+
+    private String message;
+
+    private List<String> errors;
+}
