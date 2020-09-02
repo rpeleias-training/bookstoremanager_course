2f93318fbfc4b8a01f7d29b0dbe29eb1e0179632
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-09-02 10:10:37 -0300

    Created base authors structure classes

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/author/controller/AuthorController.java
@@ -0,0 +1,18 @@
+package com.rodrigopeleias.bookstoremanager.author.controller;
+
+import com.rodrigopeleias.bookstoremanager.author.service.AuthorService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/authors")
+public class AuthorController implements AuthorControllerDocs{
+
+    private AuthorService authorService;
+
+    @Autowired
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+}
