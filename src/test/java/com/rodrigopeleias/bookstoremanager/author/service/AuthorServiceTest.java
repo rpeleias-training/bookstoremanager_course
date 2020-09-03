5bb6aa77491ffec8a3a6fb7872c7b7dd1427525d
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-09-03 09:59:15 -0300

    Create test structure class

---
+++ src/test/java/com/rodrigopeleias/bookstoremanager/author/service/AuthorServiceTest.java
@@ -0,0 +1,30 @@
+package com.rodrigopeleias.bookstoremanager.author.service;
+
+import com.rodrigopeleias.bookstoremanager.author.builder.AuthorDTOBuilder;
+import com.rodrigopeleias.bookstoremanager.author.dto.AuthorDTO;
+import com.rodrigopeleias.bookstoremanager.author.mapper.AuthorMapper;
+import com.rodrigopeleias.bookstoremanager.author.repository.AuthorRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthorServiceTest {
+
+    private final AuthorMapper authorMapper = AuthorMapper.INSTANCE;
+
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @InjectMocks
+    private AuthorService authorService;
+
+    private AuthorDTOBuilder authorDTOBuilder;
+
+    @BeforeEach
+    void setUp() {
+        authorDTOBuilder = AuthorDTOBuilder.builder().build();
+    }
+}
