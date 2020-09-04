22a087692eed4b2c67410d3aba5aac6fa7775883
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-09-04 10:12:40 -0300

    Added annotations for Auditable config

--- src/main/java/com/rodrigopeleias/bookstoremanager/config/AuditConfig.java
+++ src/main/java/com/rodrigopeleias/bookstoremanager/config/AuditConfig.java
@@ -1,4 +1,9 @@
 package com.rodrigopeleias.bookstoremanager.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
 public class AuditConfig {
 }
