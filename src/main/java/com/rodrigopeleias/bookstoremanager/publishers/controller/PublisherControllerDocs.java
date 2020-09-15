90b07ae47741e31a58d47f0c70edafc5e0520217
Rodrigo Peleias <rodrigo.peleias@gmail.com>
2020-09-15 15:50:09 -0300

    Developed struature classes for publishers module

---
+++ src/main/java/com/rodrigopeleias/bookstoremanager/publishers/controller/PublisherControllerDocs.java
@@ -0,0 +1,7 @@
+package com.rodrigopeleias.bookstoremanager.publishers.controller;
+
+import io.swagger.annotations.Api;
+
+@Api("Publishers management")
+public interface PublisherControllerDocs {
+}
---
+++ src/test/java/com/rodrigopeleias/bookstoremanager/publishers/service/PublisherServiceTest.java
@@ -0,0 +1,29 @@
+package com.rodrigopeleias.bookstoremanager.publishers.service;
+
+import com.rodrigopeleias.bookstoremanager.publishers.builder.PublisherDTOBuilder;
+import com.rodrigopeleias.bookstoremanager.publishers.mapper.PublisherMapper;
+import com.rodrigopeleias.bookstoremanager.publishers.repository.PublisherRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class PublisherServiceTest {
+
+    private final PublisherMapper publisherMapper = PublisherMapper.INSTANCE;
+
+    @Mock
+    private PublisherRepository publisherRepository;
+
+    @InjectMocks
+    private PublisherService publisherService;
+
+    private PublisherDTOBuilder publisherDTOBuilder;
+
+    @BeforeEach
+    void setUp() {
+        publisherDTOBuilder = PublisherDTOBuilder.builder().build();
+    }
+}
